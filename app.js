17a77f03750abf7ed2fe85ff706dff76783032a1
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-07-22 11:28:49 +0530

    2nd commit

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.post("/", function(req,res){
 // );
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
         console.log("server is runnning on port 3000");
 });
