0477d20f59b2e4648c1840891cd12a672bd68e82
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-07-26 21:12:22 +0530

    commit

--- app.js
+++ app.js
@@ -16,28 +16,6 @@ app.get("/", function(req,res){
 app.post("/", function(req,res){
     
         const url = req.body.repoUrl;
-<<<<<<< Updated upstream
-
-        console.log(url);
-        // let installed = true;
-        // nodeCmd.run('snyk');
-        // nodeCmd.run('snyk -v', (err, data, stderr)=>{
-        //         if(data.search('No such file or directory') != -1){
-        //                 installed = false;
-        //         }
-        // })
-        
-        // if(installed === false){
-             nodeCmd.runSync('npm install -g snyk');
-        //      console.log(installed);
-        // }
-  
-        // nodeCmd.run('snyk -v', (err, data, stderr)=>{
-        //         console.log(data);
-        // })
-        nodeCmd.runSync('snyk auth 3fb8d4f3-00eb-49cc-ba59-21b028e30dc8');
-        const synkTest = 'snyk test --json ' + url;
-=======
         const nodeCmd = require('node-cmd');
         nodeCmd.runSync('npm install -g snyk');
         nodeCmd.runSync('snyk auth 3fb8d4f3-00eb-49cc-ba59-21b028e30dc8');
@@ -109,7 +87,6 @@ app.post("/", function(req,res){
             },
             OAUth: "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"
         }
->>>>>>> Stashed changes
         
         https.get(option,(response) => {
                 let body = "";
